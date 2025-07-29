ff4fbd1479a5c3e4f16918455a821bc41615b841
Max Mlewa <maxmlewa26@gmail.com>
2025-07-29 18:57:57 -0400

    Step counter implemented and tested

--- sim/testbenches/testbench_step_counter.v
+++ sim/testbenches/testbench_step_counter.v
@@ -19,7 +19,109 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module testbench_step_counter(
+module testbench_step_counter( );
 
+    // DUT input
+    reg clk;
+    reg rst;
+    reg tick;
+    reg [3:0] steps;
+    
+    // DUT output
+    wire [3:0] count;
+    
+    // variable declarations
+    integer i;
+    integer errors;
+    
+    // DUT instantiation
+    step_counter DUT_STEP_COUNTER(
+        .clk(clk),
+        .rst(rst),
+        .tick(tick),
+        .steps(steps),
+        .count(count)
     );
+    
+    // Clock generation
+    initial
+        clk = 1'b0;
+    always
+        #5 clk = ~clk; // simulate the 10ns period
+    
+    // task for tick pulses
+    task pulse_tick;
+        begin
+            tick = 1'b1;
+            #10;
+            tick = 1'b0;
+        end
+    endtask
+    
+    // task for count value validation
+    task check_count;
+        input [3:0] expected;
+        begin
+            #1;
+            if(count !== expected)
+            begin
+                $display("FAIL: count = %d | expected = %d", count, expected);
+                errors = errors + 1;
+            end
+            
+            else
+            begin
+                $display("PASS: count = %d", count);
+            end
+        end
+    endtask
+    
+    // test sequence
+    initial
+    begin
+        errors = 0;
+        $display("\n\n=== Step Counter Tests ===");
+        
+        // initial system reset
+        rst = 1; tick = 0; steps = 4'd5; #20;
+        rst = 0;
+        
+        $display("Step size 5");
+        // step size = 5, expect count : 0,1,2,3,4,0
+        for (i =0; i<6; i = i+1)
+        begin
+            pulse_tick();
+            check_count((i+1) % 5); // expected value after wrap
+        end
+        
+        $display("Step size 3");
+        // step size 3
+        steps = 4'd3; #10;
+        pulse_tick(); check_count(2);
+        pulse_tick(); check_count(0);
+        pulse_tick(); check_count(1); // wrap
+        pulse_tick(); check_count(2);
+        pulse_tick(); check_count(0);
+        
+        $display("Step size 0");
+        // step size = 0
+        steps = 4'b0;
+        rst = 1; #10; rst = 0;
+        check_count(0);
+        
+        repeat(3)
+        begin
+            pulse_tick();
+            check_count(0);
+        end
+        
+        $display("Final reset");
+        // final reset
+        rst = 1; #10; rst = 0;
+        check_count(0);
+        
+        $display("=== Step counter Complete with %2d errors ===\n\n", errors);
+        $finish;
+    end
+    
 endmodule
--- src/step_counter.v
+++ src/step_counter.v
@@ -16,6 +16,27 @@
 
 
 module step_counter(
-
+    input wire clk, rst, tick,
+    input wire [3:0] steps,
+    output reg [3:0] count
     );
+    // the tick decouples the counter from external pulse
+    // the tick period is longer than the clk period
+    always @(posedge clk or posedge rst)
+    begin
+        if(rst)
+        begin
+            count <= 4'b0;
+        end
+        
+        else if(tick)
+        begin
+            if (steps == 4'b0)
+                count <= 4'b0;
+            else if(count >= steps -1)
+                count <= 4'b0;
+            else
+                count <= count + 1;
+        end
+    end
 endmodule
