4bf70dc97520e333ac31b4090df61adfef0b67d7
Max Mlewa <maxmlewa26@gmail.com>
2025-07-29 15:49:22 -0400

    Seven segment display decoder implementation and test results

--- sim/testbenches/testbench_seven_seg_display.v
+++ sim/testbenches/testbench_seven_seg_display.v
@@ -17,7 +17,80 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module testbench_seven_seg_display(
+module testbench_seven_seg_display( );
+    
+    // DUT input
+    reg [3:0] value;
 
+    // DUT output
+    wire [6:0] seg;
+    wire [3:0] an;
+    
+    // variable declarations
+    integer i;
+    integer errors;
+    reg [6:0] expected [0:11]; // for automatic checking
+    
+    initial
+    begin
+        expected[0] = 7'b0000001;
+        expected[1] = 7'b1001111;
+        expected[2] = 7'b0010010;
+        expected[3] = 7'b0000110;
+        expected[4] = 7'b1001100;
+        expected[5] = 7'b0100100;
+        expected[6] = 7'b0100000;
+        expected[7] = 7'b0001111;
+        expected[8] = 7'b0000000;
+        expected[9] = 7'b0000100;
+        expected[10] = 7'b0001000;
+        expected[11] = 7'b1111111;
+    end
+    
+    // DUT instantiation
+    seven_seg_display DUT_SEVEN_SEG_DISPLAY(
+        .value(value),
+        .seg(seg),
+        .an(an)
     );
+    
+    // task for a single test
+    task check_segments;
+        input [3:0] val;
+        input [6:0] actual;
+        input [6:0] exp;
+        begin
+            if (actual !== exp)
+            begin
+                $display("FAIL: value = %2d | seg = %b | expected = %b", val, actual, exp);
+                errors = errors +1;
+            end
+            
+            else
+            begin
+                $display("PASS: value = %2d | seg = %b", val, actual);
+            end
+        end
+    endtask
+    
+    // test sequence
+    initial
+    begin
+        $display("\n\n=== Seven Segment Display Tests ===");
+        
+        errors = 0;
+        for(i = 0; i <= 15; i = i +1)
+        begin
+            value = i[3:0];
+            #10;
+            if (i<= 10)
+                check_segments(value, seg, expected[i]);
+            else
+                check_segments(value, seg, expected[11]);
+        end
+        
+        $display("=== Seven Segment Display Tests Complete with %2d errors ===\n\n", errors);
+        $finish;
+    end
+    
 endmodule
--- src/seven_seg_display.v
+++ src/seven_seg_display.v
@@ -16,6 +16,29 @@
 
 
 module seven_seg_display(
-
+    input wire [3:0] value,
+    output reg [6:0] seg,
+    output wire [3:0] an
     );
+    
+    assign an = 4'b1110; // Only make the rightmost section active (an[0] low)
+    
+    always@*
+    begin
+        case(value)
+            4'd0: seg = 7'b0000001;
+            4'd1: seg = 7'b1001111;
+            4'd2: seg = 7'b0010010;
+            4'd3: seg = 7'b0000110;
+            4'd4: seg = 7'b1001100;
+            4'd5: seg = 7'b0100100;
+            4'd6: seg = 7'b0100000;
+            4'd7: seg = 7'b0001111;
+            4'd8: seg = 7'b0000000;
+            4'd9: seg = 7'b0000100;
+            4'd10: seg = 7'b0001000; // using A for 10
+            default: seg = 7'b1111111; // off
+        endcase
+        
+    end
 endmodule
