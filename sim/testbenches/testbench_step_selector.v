6036b247a0ca7fab49932d010a61400db3ca0932
Max Mlewa <maxmlewa26@gmail.com>
2025-07-29 17:31:48 -0400

    Implemented and tested the  step selector module

--- sim/testbenches/testbench_step_selector.v
+++ sim/testbenches/testbench_step_selector.v
@@ -19,7 +19,68 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module testbench_step_selector(
+module testbench_step_selector( );
 
+    // DUT input
+    reg [9:0] sw;
+    
+    // DUT output
+    wire [3:0] steps;
+    
+    // variable declarations
+    integer errors;
+    
+    // DUT instantiation
+    step_selector DUT_STEP_SELECTOR(
+        .sw(sw),
+        .steps(steps)
     );
+    
+    // task for a single test
+    task check_steps;
+        input [9:0] sw_input;
+        input [3:0] expected;
+        
+        begin
+            sw = sw_input;
+            #5;
+            if (steps !== expected)
+            begin
+                $display("FAIL: sw = %b | steps = %d | expected = %d", sw, steps, expected);
+                errors = errors + 1;
+            end
+            else
+                $display("PASS: sw = %b | steps = %d ", sw, steps);
+        end
+    endtask
+    
+    // task sequence
+    initial
+    begin
+        $display("\n\n=== Step Selector Tests ===");
+        errors = 0;
+        
+        // single switch active
+        check_steps(10'b0000000001, 4'd1);
+        check_steps(10'b0000000010, 4'd2);
+        check_steps(10'b000000010x, 4'd3);
+        check_steps(10'b00000010xz, 4'd4);
+        check_steps(10'b0000010000, 4'd5);
+        check_steps(10'b0000101010, 4'd6);
+        check_steps(10'b00010000xx, 4'd7);
+        check_steps(10'b0010000000, 4'd8);
+        check_steps(10'b0100000000, 4'd9);
+        check_steps(10'b1000000000, 4'd10);
+        
+        // multiple switches active
+        check_steps(10'b1111111111, 4'd10);
+        check_steps(10'b0001110000, 4'd7);
+        check_steps(10'b0000001111, 4'd4);
+        check_steps(10'b0000000001, 4'd1);
+        
+        // no switch active
+        check_steps(10'b0000000000, 4'd0);
+        
+        $display("=== Step Selector Complete with %2d errors ===\n\n", errors);
+    end
 endmodule
--- src/step_selector.v
+++ src/step_selector.v
@@ -16,6 +16,24 @@
 
 
 module step_selector(
-
+    input wire [9:0] sw,
+    output reg [3:0] steps
     );
+    
+    always@(*)
+    begin
+        casex(sw)                          // priority encoding for the step selection operation
+            10'b1xxxxxxxxx: steps = 4'd10; // SW9
+            10'b01xxxxxxxx: steps = 4'd9;
+            10'b001xxxxxxx: steps = 4'd8;
+            10'b0001xxxxxx: steps = 4'd7;
+            10'b00001xxxxx: steps = 4'd6;
+            10'b000001xxxx: steps = 4'd5;
+            10'b0000001xxx: steps = 4'd4;
+            10'b00000001xx: steps = 4'd3;
+            10'b000000001x: steps = 4'd2;
+            10'b0000000001: steps = 4'd1;
+            default:        steps = 4'd0; // no switches on
+        endcase
+    end
 endmodule
