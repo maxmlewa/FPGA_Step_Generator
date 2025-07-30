37591b3908e0ad049930b5455b118e952a3887dc
Max Mlewa <maxmlewa26@gmail.com>
2025-07-30 02:20:15 -0400

    Implemented and tested the debounced edge detector module

--- sim/testbenches/testbench_edge_detector_debounce.v
+++ sim/testbenches/testbench_edge_detector_debounce.v
@@ -25,7 +25,120 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module testbench_edge_detector_debounced(
+module testbench_edge_detector_debounced( );
 
+    // DUT input
+    reg clk, rst, noisy_in;
+   
+    // DUT output
+    wire tick;
+    
+    // declare other variables
+    integer i;
+    
+    // DUT instantiation
+    edge_detector_debounced #(.STABLE_COUNT(3)) DUT_EDGE_DET_DEBOUNCED (
+        .clk(clk),
+        .rst(rst),
+        .noisy_in(noisy_in),
+        .tick(tick)
     );
+    
+    // Clock generation
+    initial
+        clk = 0;
+    always
+        #5 clk = ~clk; // period = 10ns
+        
+        
+    // task simulating a rising edge with bounce
+    task noisy_rising_edge;
+        begin
+            $display("\n[Noisy Rising Edge Test]");
+            
+            noisy_in = 0;
+            #(10*8); // 8 clock periods
+            
+            // bounce for half the time 
+            for (i = 0; i<8; i = i + 1)
+            begin
+                noisy_in = ~noisy_in;
+                #5;
+            end
+            
+            // settle high
+            noisy_in = 1;
+            #(10*10);
+            
+            
+            
+        end
+    endtask
+    
+    // task simulating glitch in between pulses
+    task glitch_btwn_pulses;
+        begin
+            $display("\n [Glitch Between Pulses]");
+            
+            // real rising edge
+            noisy_in = 0;
+            #(10*8);
+            noisy_in = 1;
+            #(10*10);
+            noisy_in = 0;
+            #(10*10);
+            
+            // short glitch
+            noisy_in = 1;
+            #10;
+            noisy_in = 0;
+            #(10*10);
+            
+            // second valid pulse
+            noisy_in = 1;
+            #(10*10);
+            
+            // glitch down
+            noisy_in = 0;
+            #10;
+            noisy_in = 1;
+            #(10*10);
+            
+        end
+    endtask
+    
+    // test sequence
+    initial
+    begin
+        $display("\n\n=== Debounced Edge Detector Tests ===");
+        
+        // Reset
+        rst = 1; noisy_in = 0;
+        #20; rst = 0;
+        
+        // running test cases
+        noisy_rising_edge();
+        glitch_btwn_pulses();
+        
+        // Reset 2
+        rst = 1; noisy_in = 0;
+        #20; rst = 0;
+        
+        // iteration
+        noisy_rising_edge();
+        glitch_btwn_pulses();
+        
+        // Watch ticks
+        repeat(60)
+        begin
+            @(posedge clk);
+            $display("Time: %t | noisy_in: %b | tick: %b", $time, noisy_in, tick);
+        end
+        
+        
+        
+        
+        $display("=== Tests Complete ===\n\n");
+        $finish;
+    end
 endmodule
--- src/edge_detector_debounced.v
+++ src/edge_detector_debounced.v
@@ -15,7 +15,51 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module edge_detector_debounced(
+module edge_detector_debounced #(
+    parameter STABLE_COUNT = 8  // number of stable samples required
+)    
+(
+    input wire clk, rst, noisy_in,
+    output reg tick      
+);
 
-    );
+    reg [STABLE_COUNT -1:0] shift_reg; // shift register for trackin stability
+                                       // can be replaced with a simple integer variable
+    reg debounced = 0;
+    reg debounced_prev = 0;
+    
+    always@(posedge clk or posedge rst)
+    begin
+        if(rst)
+        begin
+            shift_reg <= 0;
+            debounced <= 0;
+            debounced_prev <= 0;
+            tick <= 0;
+        end
+        
+        else
+        begin
+            shift_reg <= {shift_reg[STABLE_COUNT - 2:0], noisy_in};
+            
+            // stable case 1
+            if (&shift_reg)
+            begin
+                debounced <= 1;
+            end
+            
+            // stable case 0
+            else if (~|shift_reg)
+            begin
+                debounced <= 0;
+            end
+            
+            // Edge detection
+            debounced_prev <= debounced;
+            tick <= (debounced == 1) && (debounced_prev == 0); // rising edge
+            
+            
+        end
+        
+    end
 endmodule
